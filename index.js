573fae8067b5bf1151450add7b761c2126dc21d6
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2022-07-15 18:47:38 +0300

    uibuild

--- index.js
+++ index.js
@@ -115,7 +115,8 @@ app.delete(`${BASEURL}/:id`, (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3002;
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
